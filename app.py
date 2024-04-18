b6b863696d66c9dcffb3c73dbfb906d986aee177
ashish <ashishpie@gmail.com>
2024-04-18 16:11:31 +0530

    first commit

--- app.py
+++ app.py
@@ -2,13 +2,25 @@ from flask import Flask, render_template, request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
+import sqlite3
 
 app = Flask(__name__)
+conn = sqlite3.connect('scraped_data.db')
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS scraped_data (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    rating TEXT,
+    address TEXT,
+    phone TEXT
+)
+''')
+conn.commit()
 
-def scrape_google_local_services(city,search_key,scrape_page):
+def scrape_google_local_services(city, search_key, scrape_page):
+    
     driver = webdriver.Chrome()
 
     # Open the Google Local Services page
@@ -31,7 +43,7 @@ def scrape_google_local_services(city,search_key,scrape_page):
                 driver.execute_script("arguments[0].scrollIntoView();", element)
                 item['name'] = element.find_element(By.CSS_SELECTOR, '.rgnuSb').text.strip()
                 item['rating'] = element.find_element(By.CSS_SELECTOR, '.OJbIQb').text
-                item['addresses'] = element.find_element(By.XPATH, './/span[2][contains(@class, "hGz87c")]').text
+                item['addresses'] = element.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[1]/div[1]/div/div/div/div[2]/div[3]/span[2]/span').text.strip()
                 item['phones'] = element.find_element(By.XPATH, './/span[3][contains(@class, "hGz87c")]').text
                 data.append(item)
             except Exception as e:
@@ -47,10 +59,100 @@ def scrape_google_local_services(city,search_key,scrape_page):
             scrape_page()
         except:
             break
-    
+ 
     driver.quit() 
+
+    #Store scraped data in the database
+    for result in data:
+        cursor.execute('''
+        INSERT INTO scraped_data (name, rating, address, phone)
+        VALUES (?, ?, ?, ?)
+        ''', (result['name'], result['rating'], result['address'], result['phone']))
+    conn.commit()
+
     return data
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/scraped", methods=['POST'])
+def scraped():
+    if request.method == "POST":
+        city = request.form['city']
+        search_key = request.form['search_key']
+        scrape_page = request.form.get('scrape_page')
+        search_key += " " + city  # Concatenate city name to the search key
+        results = scrape_google_local_services(city, search_key, scrape_page)
+        return render_template('scraped_data.html', results=results)
+
+
+def display():
+    cursor.execute('SELECT * FROM scraped_data')
+    results = cursor.fetchall()
+    return  results
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+  # addresses = element.find_element(By.XPATH, './/span[2][contains(@class, "hGz87c")]').text.strip()
+                # if addresses:
+                #      for address in addresses.split(", "): 
+                #          print("Address:", address)
+                # item['addresses'] = addresses if ", " in addresses else "" 
+
+
+
+
+
+
+
+'''def scrape_google_local_services(city,search_key,scrape_page):
+    driver = webdriver.Chrome()
+
+    # Open the Google Local Services page
+    driver.get('https://www.google.com/localservices/prolist?g2lbs=AIQllVzWSYspFtzaywPV9jv6ozuwdPjt7hfraOfChGkHnMWRs6jF0EN0JnmLVAxCXczEjUzv4oHOVvTL6BdV-38uYjJ1IChxr0ZEAtiL7qHihksvHmIrYX9YXjZaRJZM3RoWbXTEIRAz&hl=en-IN&gl=in&ssta=1&q=digital%20marketing%20agency&oq=digital%20marketing%20agency&slp=MgA6HENoTUlwNk9mdTl6a2hBTVZCUS1EQXgzU1FBV19SAggCYACSAbgCCg0vZy8xMWg1N2sxNTMxCg0vZy8xMWJ3NjlyemhwCg0vZy8xMXFwbTVoY2YyCg0vZy8xMWprNHZ2NXQ1Cg0vZy8xMXM2MXN3ZDg3Cg0vZy8xMXZqaG1tbGZtCg0vZy8xMWZzdm01XzF5Cg0vZy8xMWxjZnE0eV9yCg0vZy8xMWtqajcycnQzCg0vZy8xMXJzYnIzcnc5Cg0vZy8xMWozNDQ4OHE4Cg0vZy8xMXNzd3BzZnAzCg0vZy8xMWYzam15M2puCg0vZy8xMWhmNHJfcThyCg0vZy8xMW4xNTB6aHNoCg0vZy8xMWM2N3dtOHRmCg0vZy8xMWRkeGJ6cmNsCg0vZy8xMWg0X2xzMHc0Cg0vZy8xMWoyZnA0bWIyCg0vZy8xMXY1eDM0OGpzEgQSAggBEgQKAggBmgEGCgIXGRAA&src=2&serdesk=1&sa=X&ved=2ahUKEwiurZS73OSEAxX91TgGHYHWCWYQjGp6BAgiEAE&scp=ChVnY2lkOm1hcmtldGluZ19hZ2VuY3kSVxISCRcp3KaMReA7EXyOXMDrZ7gEGhIJYxUdQVlO4DsRQrA4CSlYRf4iFVN1cmF0LCBHdWphcmF0IDM5NTAwNioUDfIgoAwVUZVqKx3Upq4MJZ0nfiswARoYZGlnaXRhbCBtYXJrZXRpbmcgYWdlbmN5IhhkaWdpdGFsIG1hcmtldGluZyBhZ2VuY3kqEE1hcmtldGluZyBhZ2VuY3k%3D')  
+
+    # Find the search input element and input the search query
+    search_input = driver.find_element(By.CSS_SELECTOR, 'input[class="MDhB7"]')
+    search_input.clear()  # Clear any existing text in the search input
+    search_input.send_keys(search_key, Keys.ENTER)
+
+    time.sleep(2)  # Wait for the results to load
+    data = []
+    def scrape_page():
+    # Collect data
+        nonlocal data 
+        elements = driver.find_elements(By.CSS_SELECTOR, '[class="NwqBmc"]')
+        for element in elements:
+            item = {}
+            try:
+                driver.execute_script("arguments[0].scrollIntoView();", element)
+                item['name'] = element.find_element(By.CSS_SELECTOR, '.rgnuSb').text.strip()
+                item['rating'] = element.find_element(By.CSS_SELECTOR, '.OJbIQb').text
+                item['addresses'] = element.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[3]/div/div/div[1]/div[3]/div[3]/c-wiz/div/div/div[1]/c-wiz/div/div[1]/div[1]/div/div/div/div[2]/div[3]/span[2]/span').text.strip()
+                item['phones'] = element.find_element(By.XPATH, './/span[3][contains(@class, "hGz87c")]').text
+                data.append(item)
+            except Exception as e:
+                print("Error occurred:", e)
+
+
+    scrape_page()
+    while True:   
+        try:
+            button = driver.find_elements(By.XPATH, '//button[@aria-label="Next"]')[1]
+            button.click()
+            time.sleep(2)
+            scrape_page()
+        except:
+            break
+ 
+    driver.quit() 
+    return data
+    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -65,38 +167,24 @@ def scraped():
         results = scrape_google_local_services(city, search_key,scrape_page)
         return render_template('scraped_data.html', results=results)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    '''
 
-'''Name: Gold Star Refrigeration Service
-Rating: 4.8
-Address: 15+ years in business
-Address: Surat, Gujarat
-Phone: 099258 90932
 
-Name: jariwala care home service
-Rating: 4.5
-Address: 093279 78075
 
-Name: The Repairing Company
-Rating: 4.9
-Address: Surat, Gujarat
-Phone: 099093 69569
 
-Name: Star-Win Enterprise
-Rating: 4.8
-Address: 10+ years in business
-Address: Surat, Gujarat
-Phone: 098246 11124
 
-Name: Jay khodiyar refrigeration
-Rating: 4.8
-Address: Surat, Gujarat
-Phone: 098241 11124
 
-Name: URJA CARE (Microwave Oven Servicing & Repairing)
-Rating: 4.9
-Address: 10+ years in business
-Address: Surat, Gujarat
-Phone: 094271 52339
-'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
